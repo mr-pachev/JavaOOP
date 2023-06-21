fe07adc97f91bb6f46295d5bbb693be35a767562
Ivan Pachev <mr.pachev@abv.bg>
2023-06-21 20:49:36 +0300

    Lab_HotelReservation_04

---
+++ Lab_HotelReservation_04/Season.java
@@ -0,0 +1,15 @@
+package Lab_HotelReservation_04;
+
+public enum Season {
+    Autumn(1), Spring(2), Winter(3), Summer(4);
+    private int value;
+
+    Season(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
