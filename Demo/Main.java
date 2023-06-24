27151d04fcc105e2605cfab5ed8aa1b458bfd6a6
Ivan Pachev <mr.pachev@abv.bg>
2023-06-24 13:47:48 +0300

    Update

--- Demo/Main.java
+++ Demo/Main.java
@@ -1,81 +1,98 @@
+
 package Demo;
 
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        long bagCapacity = Long.parseLong(scanner.nextLine());
+        String[] safeData = scanner.nextLine().split("\\s+");
 
-        int[] dimestionsMatrix = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int rows = dimestionsMatrix[0];
-        int cols = dimestionsMatrix[1];
+        var bag = new LinkedHashMap<String, LinkedHashMap<String, Long>>();
+        long gold = 0;
+        long gem = 0;
+        long cash = 0;
 
-        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < safeData.length; i += 2) {
 
-        fillMatrix(rows, cols, matrix);
+            String nameItem = safeData[i];
+            long currentSumItem = Long.parseLong(safeData[i + 1]);
 
-        String command = scanner.nextLine();
+            String currentItem = "";
 
-        long sum = 0;
-        while (!command.equals("Let the Force be with you")) {
-            int[] jediPosition = Arrays.stream(command.split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
-            int[] evilPosititon = Arrays.stream(scanner.nextLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
-            int currentRowEvil = evilPosititon[0];
-            int currentColEvil = evilPosititon[1];
-            evilMovement(matrix, currentRowEvil, currentColEvil);
-
-            int currentRowJedi = jediPosition[0];
-            int currentColJedi = jediPosition[1];
-            sum = getSumJediMovement(matrix, sum, currentRowJedi, currentColJedi);
-
-            command = scanner.nextLine();
-        }
-
-        System.out.println(sum);
-    }
-
-    //движение наджедая и сумиране на стойностите от клетките
-    private static long getSumJediMovement(int[][] matrix, long sum, int currentRowJedi, int currentColJedi) {
-        while (currentRowJedi >= 0 && currentColJedi < matrix[0].length) {
-            if (isValidPosition(matrix, currentRowJedi, currentColJedi)) {
-                sum += matrix[currentRowJedi][currentColJedi];
+            if (nameItem.length() == 3) {
+                currentItem = "Cash";
+            } else if (nameItem.toLowerCase().endsWith("gem")) {
+                currentItem = "Gem";
+            } else if (nameItem.toLowerCase().equals("gold")) {
+                currentItem = "Gold";
             }
-            currentColJedi++;
-            currentRowJedi--;
-        }
-        return sum;
-    }
 
-    //движение на злото
-    private static void evilMovement(int[][] matrix, int currentRowEvil, int currentColEvil) {
-        while (currentRowEvil >= 0 && currentColEvil >= 0) {
-            if (isValidPosition(matrix, currentRowEvil, currentColEvil)) {
-                matrix[currentRowEvil][currentColEvil] = 0;
+            if (currentItem.equals("")) {
+                continue;
+            } else if (bagCapacity < bag.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + currentSumItem) {
+                continue;
             }
-            currentRowEvil--;
-            currentColEvil--;
-        }
-    }
 
-    //пълнене на матрицата
-    private static void fillMatrix(int rows, int cols, int[][] matrix) {
-        int value = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] = value++;
+            switch (currentItem) {
+                case "Gem":
+                    if (!bag.containsKey(currentItem)) {
+                        if (bag.containsKey("Gold")) {
+                            if (currentSumItem > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                                continue;
+                            }
+                        } else {
+                            continue;
+                        }
+                    } else if (bag.get(currentItem).values().stream().mapToLong(e -> e).sum() + currentSumItem > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                        continue;
+                    }
+                    break;
+                case "Cash":
+                    if (!bag.containsKey(currentItem)) {
+                        if (bag.containsKey("Gem")) {
+                            if (currentSumItem > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                                continue;
+                            }
+                        } else {
+                            continue;
+                        }
+                    } else if (bag.get(currentItem).values().stream().mapToLong(e -> e).sum() + currentSumItem > bag.get("Gem").values().stream().mapToLong(e -> e).sum()) {
+                        continue;
+                    }
+                    break;
+            }
+
+            if (!bag.containsKey(currentItem)) {
+                bag.put((currentItem), new LinkedHashMap<String, Long>());
+            }
+
+            if (!bag.get(currentItem).containsKey(nameItem)) {
+                bag.get(currentItem).put(nameItem, 0L);
+            }
+
+
+            bag.get(currentItem).put(nameItem, bag.get(currentItem).get(nameItem) + currentSumItem);
+            if (currentItem.equals("Gold")) {
+                gold += currentSumItem;
+            } else if (currentItem.equals("Gem")) {
+                gem += currentSumItem;
+            } else if (currentItem.equals("Cash")) {
+                cash += currentSumItem;
             }
         }
-    }
 
-    //валидация на позицията
-    public static boolean isValidPosition(int[][] matrix, int row, int col) {
-        return row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length;
+        for (var currentBag : bag.entrySet()) {
+            Long sumValues = currentBag.getValue().values().stream().mapToLong(l -> l).sum();
+
+            System.out.println(String.format("<%s> $%s", currentBag.getKey(), sumValues));
+
+            currentBag.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
+        }
     }
 }
